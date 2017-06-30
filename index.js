0f48245165a9d2ce561a307a91685466d6b07826
John Riviello <john_riviello@comcast.com>
2017-06-30 12:08:56 -0400

    Removing config.basePath, as it's not used

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
   'use strict';
 
   var initPolymer = require('./lib/init')
-  initPolymer.$inject = ['config.files', 'config.basePath', 'config.polymer',
-                         'config.client']
+  initPolymer.$inject = ['config.files', 'config.polymer', 'config.client']
 
   module.exports = {
     'framework:polymer': ['factory', initPolymer]
